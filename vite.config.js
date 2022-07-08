226ccf7de9333dae629f6a8a1401a53cff5a05a8
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2022-07-09 01:53:10 +0400

    Installed Laravel Breeze Inertia & Vue, compiling powered by @vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server:{
+        hmr: {
+            host: "127.0.0.1",
+            port: 8000
+        }
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
