ddcb878f574b51daded3ff4cc0b1c6ddb2182eb1
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2022-07-24 18:56:21 +0400

    modified TermsList in footer, added Link urls

--- resources/js/Components/Navigation/Lists/TermsList.vue
+++ resources/js/Components/Navigation/Lists/TermsList.vue
@@ -1,24 +1,46 @@
 <script setup>
 /** Source */
 import { ref } from 'vue'
+import { getActiveLanguage } from 'laravel-vue-i18n'
+/** Components */
+import { Link } from '@inertiajs/inertia-vue3'
 /** Constants */
 const terms = ref([
-    'home',
-    'market',
-    'shop',
-    'charity',
-    'about us'
+    {
+        title: 'home',
+        url: route('home', getActiveLanguage()),
+    },
+    {
+        title: 'market',
+        url: route('market', getActiveLanguage()),
+    },
+    {
+        title: 'shop',
+        url: route('shop', getActiveLanguage()),
+    },
+    {
+        title: 'charity',
+        url: route('charity', getActiveLanguage()),
+    },
+    {
+        title: 'about us',
+        url: route('about', getActiveLanguage()),
+    }
 ])
 </script>
 <!-- Terms List Template -->
 <template>
     <ul class="footer__menu">
         <li 
-            v-for="term in terms"
-            :key="term.index"
+            v-for="item in terms"
+            :key="item.index"
             class="footer__menu--item d-flex"
         >  
-            <a href="" class="footer__menu--link regular"><span class="capitalize" style="text-transform: capitalize;">{{ term }}</span></a>
+            <Link :href="item.url" class="footer__menu--link regular">
+                <span class="capitalize" style="text-transform: capitalize;">
+                    {{ item.title }}
+                </span>
+            </Link>
         </li>
     </ul>
 </template>
