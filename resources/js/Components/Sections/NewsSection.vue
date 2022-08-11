662ba88b0083897f17aa95e3500448e2e0ef76f3
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2022-08-11 18:55:57 +0400

    converting mobile version to dynamic

--- resources/js/Components/Sections/NewsSection.vue
+++ resources/js/Components/Sections/NewsSection.vue
@@ -13,12 +13,13 @@ const { t } = useI18n({ inheritLocale: true })
     <div class="container-lg mobile-container">
         <div class="row">
             <div class="col-12 col-lg-6">
+                <!-- heading -->
                 <div class="products__section d-flex align-items-center justify-content-center justify-content-lg-between">
                     <div class="d-flex align-items-center">
                         <h2 class="products__section--text regular">{{ t("titles.news") }}</h2>
                         <a href="" class="products__section--link regular">{{ t("links.view_all") }}</a>
                     </div>
-                </div><!-- section head end -->
+                </div>
                 <!-- news desktop version -->
                 <ul class="news d-none d-lg-block">
                     <li class="d-flex" v-for="item in news" :key="item.id">
@@ -39,60 +40,15 @@ const { t } = useI18n({ inheritLocale: true })
                     <div class="news-slider">
                         <div class="splide__track">
                             <ul class="splide__list">
-                                <div class="splide__slide">
+                                <div class="splide__slide" v-for="item in news" :key="item.id">
                                     <a href="" class="news news__item d-flex flex-column">
                                         <figure class="news__item--img">
-                                            <img src="assets/img/news.png" alt="">
+                                            <img :src="item.cover" alt="">
                                         </figure>
                                         <div class="news__item--text">
-                                            <p class="news__item--date regular">
-                                                01/06/2021
-                                            </p>
-                                            <h1 class="news__item--title bold">
-                                                Elit Electronics Is Going To Joined Itemssphere 
-                                                With Most Highest Percentage OF CHARITY
-                                            </h1>
-                                            <p class="news__item--desc regular">
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nisi rem quo, nam atque tenetur dignissimos excepturi quas molestias, vero nulla alias. Nulla, quam natus. Nam, incidunt! Voluptate, debitis ducimus!
-                                            </p>
-                                        </div>
-                                    </a>
-                                </div><!-- item end -->
-                                <div class="splide__slide">
-                                    <a href="" class="news news__item d-flex flex-column">
-                                        <figure class="news__item--img">
-                                            <img src="assets/img/news.png" alt="">
-                                        </figure>
-                                        <div class="news__item--text">
-                                            <p class="news__item--date regular">
-                                                01/06/2021
-                                            </p>
-                                            <h1 class="news__item--title bold">
-                                                Elit Electronics Is Going To Joined Itemssphere 
-                                                With Most Highest Percentage OF CHARITY
-                                            </h1>
-                                            <p class="news__item--desc regular">
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nisi rem quo, nam atque tenetur dignissimos excepturi quas molestias, vero nulla alias. Nulla, quam natus. Nam, incidunt! Voluptate, debitis ducimus!
-                                            </p>
-                                        </div>
-                                    </a>
-                                </div><!-- item end -->
-                                <div class="splide__slide">
-                                    <a href="" class="news news__item d-flex flex-column">
-                                        <figure class="news__item--img">
-                                            <img src="assets/img/news.png" alt="">
-                                        </figure>
-                                        <div class="news__item--text">
-                                            <p class="news__item--date regular">
-                                                01/06/2021
-                                            </p>
-                                            <h1 class="news__item--title bold">
-                                                Elit Electronics Is Going To Joined Itemssphere 
-                                                With Most Highest Percentage OF CHARITY
-                                            </h1>
-                                            <p class="news__item--desc regular">
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nisi rem quo, nam atque tenetur dignissimos excepturi quas molestias, vero nulla alias. Nulla, quam natus. Nam, incidunt! Voluptate, debitis ducimus!
-                                            </p>
+                                            <p class="news__item--date regular">{{ item.published_at }}</p>
+                                            <h1 class="news__item--title bold">{{ item.title }}</h1>
+                                            <p class="news__item--desc regular">{{ item.descriotion }}</p>
                                         </div>
                                     </a>
                                 </div>
@@ -103,12 +59,13 @@ const { t } = useI18n({ inheritLocale: true })
             </div>
             <!-- our shops -->
             <div class="col-12 col-lg-6">
+                <!-- heading -->
                 <div class="products__section d-flex align-items-center justify-content-center justify-content-lg-between">
                     <div class="d-flex align-items-center">
                         <h2 class="products__section--text regular">{{ t("titles.our_shops") }}</h2>
                         <a href="" class="products__section--link regular">{{ t("links.view_all") }}</a>
                     </div>
-                </div><!-- section head end -->
+                </div>
                 <!-- desktop version -->
                 <ul class="ourshop d-none d-lg-flex flex-wrap">
                     <li class="ourshop__item" v-for="item in shops" :key="item.id">
@@ -122,34 +79,13 @@ const { t } = useI18n({ inheritLocale: true })
                     <div class="ourshop-slider">
                         <div class="splide__track">
                             <ul class="splide__list">
-                                <div class="splide__slide">
+                                <div class="splide__slide" v-for="item in shops" :key="item.id">
                                     <li class="">
                                         <a href="" class="ourshop__item--link">
-                                            <img src="assets/img/alta.png" alt="" class="ourshop__item--img">
+                                            <img :src="item.cover" alt="" class="ourshop__item--img">
                                         </a>
                                     </li>
-                                </div><!-- item end -->
-                                <div class="splide__slide">
-                                    <li class="">
-                                        <a href="" class="ourshop__item--link">
-                                            <img src="assets/img/alta.png" alt="" class="ourshop__item--img">
-                                        </a>
-                                    </li>
-                                </div><!-- item end -->
-                                <div class="splide__slide">
-                                    <li class="">
-                                        <a href="" class="ourshop__item--link">
-                                            <img src="assets/img/alta.png" alt="" class="ourshop__item--img">
-                                        </a>
-                                    </li>
-                                </div><!-- item end -->
-                                <div class="splide__slide">
-                                    <li class="">
-                                        <a href="" class="ourshop__item--link">
-                                            <img src="assets/img/alta.png" alt="" class="ourshop__item--img">
-                                        </a>
-                                    </li>
-                                </div><!-- item end -->
+                                </div>
                             </ul>
                         </div>
                     </div>
