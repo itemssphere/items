e505773177542adf33da63dd7d8f1dad2db3e0e1
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2022-08-24 17:04:37 +0400

    created search input component

--- resources/js/Components/Bars/Search.vue
+++ resources/js/Components/Bars/Search.vue
@@ -1,36 +1,34 @@
 <script setup>
 /** Source */
-import { ref } from 'vue'
+import { ref } from "vue"
 /** Components */
-import ApplicationLogo from '../ApplicationLogo.vue'
-import AuthorizationBlock from '../Blocks/AuthorizationBlock.vue'
-import MobileLogo from '../MobileLogo.vue'
-import SearchPopup from '../Popups/SearchPopup.vue'
+import ApplicationLogo from "../ApplicationLogo.vue";
+import AuthorizationBlock from "../Blocks/AuthorizationBlock.vue";
+import MobileLogo from "../MobileLogo.vue";
+import SearchPopup from "../Popups/SearchPopup.vue";
+import FormSearch from "../Forms/FormSearch.vue";
 /** Props */
-defineProps({
-    pageName: String
-})
+defineProps({ pageName: String })
 /** Constants */
 const searchKey = ref('')
+/** Functions */
+const search = (string) => {
+    searchKey.value = string
+}
 </script>
 <!-- Search Bar Template --->
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-2 d-none d-lg-block">
-                <ApplicationLogo />
-            </div>
-            <div class="col-10 d-none d-lg-flex justify-content-between align-items-center">
-                <form class="header__search d-flex align-items-center justify-content-between">
-                    <input v-model="searchKey" type="text" placeholder="search" class="header__search--input regular">
-                    <button class="header__search--btn">
-                        <img src="/assets/img/svg/loop.svg" alt="">
-                    </button>
-                </form>
-                <AuthorizationBlock />
-            </div>
-            <MobileLogo :pageName="pageName"/>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-2 d-none d-lg-block">
+        <ApplicationLogo />
+      </div>
+      <div class="col-10 d-none d-lg-flex justify-content-between align-items-center">
+        <FormSearch @search="search" />
+        <AuthorizationBlock />
+      </div>
+      <MobileLogo :pageName="pageName" />
     </div>
-    <SearchPopup :searchKey="searchKey" />
+  </div>
+  <SearchPopup :searchKey="searchKey" />
 </template>
