6a4280b01322d13794641abbc198df4885829653
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-01-27 02:02:58 +0400

    ProductSeeder

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
-use Illuminate\Support\Arr;
 use Illuminate\Database\Seeder;
 
 class ProductsSeeder extends Seeder
@@ -16,31 +15,7 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         /** Create Products */
         Product::factory()->count(20)->create();
-=======
-        /** Number of Products */
-        $parents = 10;
-        $secondary = 25;
-        $third = 50;
-        $fourth = 100;
-        /** Create Products */
-        Product::factory()->count($parents)->create([
-            'category_id' => 1
-        ]);
-        /** Create Products */
-        Product::factory()->count($secondary * Arr::random([2,3]))->create([
-            'category_id' => Arr::random(range(1,$parents))
-        ]);
-        /** Create Products */
-        Product::factory()->count($third * Arr::random([2,3]))->create([
-            'category_id' => Arr::random(range($parents / 2, $secondary))
-        ]);
-        /** Create Products */
-        Product::factory()->count($fourth * Arr::random([2,3]))->create([
-            'category_id' => Arr::random(range($secondary / 2, $third))
-        ]);
->>>>>>> ad9023aa5a5befee8151f7f4aa12b6349fae90a4
     }
 }
