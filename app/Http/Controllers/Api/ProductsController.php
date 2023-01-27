b3f153d29e0c3076a9a4a3d16449946f7aa6b137
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-01-27 12:45:34 +0400

    accepted current changes

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -3,15 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Product;
-<<<<<<< HEAD
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-=======
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
-use App\Http\Controllers\Controller;
->>>>>>> ad9023aa5a5befee8151f7f4aa12b6349fae90a4
 use App\Http\Resources\Api\Products\ProductsResource;
 
 class ProductsController extends Controller
@@ -23,15 +17,7 @@ class ProductsController extends Controller
      */
     public function index(): JsonResponse
     {
-<<<<<<< HEAD
         return response()->success(ProductsResource::collection(Product::customPaginate()));
-=======
-        return response()->json([
-            'success' => true,
-            'data' => ProductsResource::collection(Product::with('category')->get())
-        ], 200);
-
->>>>>>> ad9023aa5a5befee8151f7f4aa12b6349fae90a4
     }
 
     /**
