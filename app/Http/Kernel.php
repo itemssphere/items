a42719775c813656dfc3f1659a75d9a2f7cf1a0d
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-05-21 16:35:37 +0400

    Removed Inertia middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SetLanguage::class,
-            \App\Http\Middleware\HandleInertiaRequests::class,
+            // \App\Http\Middleware\HandleInertiaRequests::class,
         ],
 
         'api' => [
